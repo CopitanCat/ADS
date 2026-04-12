1334c141532abd1c14cb9bb3f11f4538952f1b72
capitancat <capitancat56@gmail.com>
2026-04-12 14:42:30 +0500

    Comments in code

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,13 +1,16 @@
 import java.util.Iterator;
 
 public class MyArrayList<T> implements MyList<T>{
+    //List of data
     private Object[] data;
+    //Size of list
     private int size;
     public MyArrayList(){
         data = new Object[10];
         size = 0;
     }
     public MyArrayList(int Size) throws IllegalAccessException {
+        //Exception in size is negative
         if(Size<=0){
             throw new IllegalAccessException("Size need to be more than 0");
         }
@@ -15,53 +18,64 @@ public class MyArrayList<T> implements MyList<T>{
         size = 0;
     }
 
+    //Grow the size of list
     private void grow() {
         int newCapacity = Math.max(data.length * 2, 10);
+        //make new array and add it to array named by data
         Object[] newData = new Object[newCapacity];
         for (int i = 0; i < size; i++) {
             newData[i] = data[i];
         }
         data = newData;
     }
+
+    //getting data by index
     @Override
     public T get(int index) {
         return (T) this.data[index];
     }
 
+    //Add element to last place
     @Override
     public void add(Object element) {
         if (size == data.length) grow();
         this.data[this.size++] = element;
     }
-
+    // Add element by index
     @Override
     public void add(Object element, int index) {
         if (size == data.length) grow();
+        // Move right elements of data from index
         for (int i = size; i > index; i--) {
             data[i] = data[i - 1];
         }
         data[index] = element;
         size++;
     }
-
+    //Return current size of list
     @Override
     public int size() {
         return this.size;
     }
 
+    //removing data by index
     @Override
     public void remove(int index) {
+        //Moving element of data to left
         for (int i = index; i < size - 1; i++) {
             data[i] = data[i + 1];
         }
+        //Determine element in size as null and reduce the size
         data[--size] = null;
     }
 
+    //Change index element to the item
     @Override
     public void set(int index, Object item) {
        data[index] = item;
     }
 
+    //Returns an iterator over the elements in this list in proper sequence
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
--- src/MyList.java
+++ src/MyList.java
@@ -1,8 +1,19 @@
 public interface MyList<T> extends Iterable<T>{
+    //getting element by index
     T get(int index);
+
+    // Append the element
     void add(T element);
+
+    // Add element to specify index
     void add(T element , int index);
+
+    // Return size of list
     int size();
+
+    // removing element by index
     void remove(int index);
+
+    // Replaces the element at the specified position with the given item.
     void set(int index, T item);
 }
